86abda14784dc47e9150d97dd58ec0aa28b8eb69
eduncan <eduncan@eduncan-Linux.(none)>
2014-09-28 19:48:06 -0400

    reads important bits from yaml

--- scripts/path.py
+++ scripts/path.py
@@ -7,12 +7,23 @@ class pathNavigation:
 
 	def __init__(self):
 		#Get map data
-		rospy.wait_for_service("static_map")
-		static_map = rospy.ServiceProxy("static_map", GetMap)
-		try:
-			map = static_map().map
-		except:
-			print "error receiving map"
+		# rospy.wait_for_service("static_map")
+		# static_map = rospy.ServiceProxy("static_map", GetMap)
+		# try:
+		# 	map = static_map().map
+		# except:
+		# 	print "error receiving map"
+
+		file = open('/home/eduncan/mymap4.yaml', 'r')
+		contents =  file.read()
+		temp = contents.split("image: ")
+		image = temp[1].split("\n")
+		print image[0]
+		temp = contents.split("origin: ")
+		origin = temp[1].split("\n")
+		print origin[0]
+
+
 
 	def process(self):
 		print "map"
