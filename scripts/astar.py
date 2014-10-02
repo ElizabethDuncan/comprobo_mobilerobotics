dfbca284f4d0f2c291defd6498567499aa93bd8d
eduncan <eduncan@eduncan-Linux.(none)>
2014-10-02 14:36:57 -0400

    preliminary path work

--- scripts/astar.py
+++ scripts/astar.py
@@ -1,7 +1,19 @@
-class Node():
-  def __init__(self):
-    pass
+#!/usr/bin/python
 
+class Node(parent, x, y):
+  def __init__(self):
+    self.x = x
+    self.y = y
+    self.parent = parent
+    if self.parent != None:
+      self.parent.assign_child(child)
+    self.children = []
+
+  def return_parent(self):
+    return self.parent
+
+  def assign_child(self, child):
+    self.children.append(child)
 
 class Queue():
   def __init__(self):
@@ -33,3 +45,12 @@ def bfs(graph, start, path = []):
 def expand_node(node, graph, queue):
   for each in graph[node]:
     queue.push(each)
+
+
+# root = Node(None, -51, -51)
+# next_right = Node(root, -54, -55)
+# next_left = Node(root, -38, -55)
+# next_next_right = Node(next_right, -60, -60)
+
+# parent = next_next_right.return_parent()
+# print parent.x
